c82bfcbb20f924b9e7d42fa697918ab8ef97f8b1
Geeru4 <111010523+Geeru4@users.noreply.github.com>
2023-01-24 20:12:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let input = document.createElement(tag = "input", properties = [["innerHTML", "Walter White"], ["type", "text"], ["placeholder", "im cooking"]], styles = [["fontSize", "18px"], ["color", "green"]]);
-for (let propertie of properties) {
-    propertie.style = [['fontSize','65px'], ['backgroundColor', 'red']];
-
-    console.log("cooking")
-}
-
-const heisenberg = document.querySelector('.Heisenberg');
-heisenberg.append(input);
-
-console.log("proverka appenda")
+function createElement(tag,properties,styles,parentElem) {
+    let hate = parentElem
+    let sergey = document.createElement(tag)
+    for (let propertie of properties) {
+        sergey[propertie[0]] = propertie[1];
+        console.log(propertie,propertie[0],propertie[1])
+        
+        //propertie.style = [['fontSize','65px'], ['backgroundColor', 'red']];
+    };
+    for (let style of styles) {
+        sergey.style[style[0]] = style[1];
+    };
+    hate.appendChild(sergey);
+};
+createElement(tag = "input", properties = [["innerHTML", "Walter White"], ["type", "text"], ["placeholder", "im cooking"]], styles = [["fontSize", "18px"], ["color", "green"] ],parentElem = document.querySelector('.Heisenberg'));
